c1713dca7d230b12b8a157b94dad117e8395a3cb
Tengen <d.stalnov@mail.ru>
2013-06-28 18:31:23 +1000

    it's impossible

---
+++ OK/index.php
@@ -0,0 +1,17 @@
+<HTML>
+<head>
+<link href="http://www.odnoklassniki.ru/oauth/resources.do?type=css" rel="stylesheet">
+
+<title>Одноклассники</title>
+</head>
+<body>
+<script src="http://www.odnoklassniki.ru/oauth/resources.do?type=js" type="text/javascript" charset="utf-8">
+</script>
+
+<a class="odkl-oauth-lnk" 
+     href="" 
+     onclick="ODKL.Oauth2(this, 184970496, 'SET STATUS;VALUABLE ACCESS', 'list.php' );return false;">
+</a>
+<a href="list.php">друзья</a>
+</body>
+</HTML>
---
+++ OK/list.php
@@ -0,0 +1,16 @@
+<?php
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ns2:friends_get_response xmlns:ns2="http://api.forticom.com/1.0/">
+ <uid>CBAOGHAMABABABABA</uid>
+ <uid>212DA7CE3A580D2BFC60DF19</uid>
+</ns2:friends_get_response>
+?>
+
+<html>
+<head>
+<title>однолассники</title>
+</head>
+<body>
+
+</body>
+</html>
